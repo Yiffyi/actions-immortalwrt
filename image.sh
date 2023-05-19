c94f2101652d4a13a7b035c4bb1868129316723a
Yiffyi Jia <daniel_sy@live.com>
2023-05-20 00:00:31 +0800

    Update image.sh

--- image.sh
+++ image.sh
@@ -1,1 +1,1 @@
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="luci luci-app-passwall luci-i18n-passwall-zh-cn" FILES="files"
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
