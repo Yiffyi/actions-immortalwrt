142b3d9b54eee4658cf287f70c170dadb2eb3edd
Yiffyi Jia <daniel_sy@live.com>
2023-05-20 16:42:26 +0000

    trim down config; use newer package for golang

--- package.sh
+++ package.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
+
 make defconfig
 make -j package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
