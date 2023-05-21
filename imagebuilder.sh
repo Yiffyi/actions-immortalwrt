0e40348e8f1112741ed0d581901e711f77cd62f4
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 13:46:13 +0000

    try: dnsmasq v2.89 from snapshot

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -12,4 +12,4 @@ echo "src/gz immortalwrt_packages https://downloads.immortalwrt.org/releases/21.
 # echo "src/gz immortalwrt_luci https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/luci" >> repositories.conf.new
 mv repositories.conf.new repositories.conf
 
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq dnsmasq-full luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/base/dnsmasq-full_2.89-4_aarch64_cortex-a53.ipk luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
