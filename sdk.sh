7bd80665248c565ff8bb21f12fb16d2c675a0f2c
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 16:28:45 +0800

    try again: IGNORE_ERRORS

--- sdk.sh
+++ sdk.sh
@@ -3,4 +3,4 @@
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
 
 make defconfig
-IGNORE_ERRORS=1 make -j IGNORE_ERRORS=m package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
+IGNORE_ERRORS=1 make -j || make -j1 V=s
