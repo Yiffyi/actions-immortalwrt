e44fad622bcf0ee0fb34029dc59abcfad6dabccf
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 16:41:02 +0000

    fix: boost

--- sdk.sh
+++ sdk.sh
@@ -2,4 +2,5 @@
 
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
 
-IGNORE_ERRORS=1 make -j$(nproc) package/luci-app-passwall/compile || make -j1 V=s
+# IGNORE_ERRORS=1
+make -j$(nproc) package/luci-app-passwall/compile || make -j1 V=s
