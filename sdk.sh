ce9885ed0565d031225089220c3bf35758800891
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 04:49:57 +0000

    try ignore error

--- sdk.sh
+++ sdk.sh
@@ -3,4 +3,4 @@
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
 
 make defconfig
-make -j package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
+make -j IGNORE_ERRORS=m package/luci-app-passwall/compile || make -j1 IGNORE_ERRORS=m package/luci-app-passwall/compile V=s
