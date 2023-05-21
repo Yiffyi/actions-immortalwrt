5bd5009313a1f457223e3dd4cb37e74059032b4b
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 01:47:43 +0000

    fix: keyring

--- image.sh
+++ image.sh
@@ -1,4 +1,7 @@
 echo "src/gz immortalwrt_2102_packages https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/packages" | cat - /workdir/imgbuilder/repositories.conf > /workdir/imgbuilder/repositories.conf.new
 mv /workdir/imgbuilder/repositories.conf.new /workdir/imgbuilder/repositories.conf
 
+git clone --depth=1 https://github.com/immortalwrt/keyring /workdir/keyring
+rm -r keys && ln -sf /workdir/keyring/usign keys
+
 make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
