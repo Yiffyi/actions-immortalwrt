0a5d7d2b4f21c7b53509aef4b4ea0fb29b157533
Yiffyi Jia <daniel_sy@live.com>
2023-05-21 01:27:33 +0000

    test

--- image.sh
+++ image.sh
@@ -1,1 +1,4 @@
+echo "src/gz immortalwrt_2102_packages https://downloads.immortalwrt.org/releases/21.02-SNAPSHOT/packages/aarch64_cortex-a53/packages" | cat - /workdir/imgbuilder/repositories.conf > /workdir/imgbuilder/repositories.conf.new
+mv /workdir/imgbuilder/repositories.conf.new /workdir/imgbuilder/repositories.conf
+
 make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+exit
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
 
 make defconfig
