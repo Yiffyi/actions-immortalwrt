041ba656e2812f72d5daddc8e8ceda4b472d24c9
Yiffyi Jia <daniel_sy@live.com>
2023-05-22 01:31:26 +0000

    config debugging

--- sdk.sh
+++ sdk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
-
+make defconfig
+cat .config
 # IGNORE_ERRORS=1
 make -j$(nproc) package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
