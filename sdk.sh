f55b5645d53d49bec13798fa77ff4c8a2fe7b56e
Yiffyi Jia <daniel_sy@live.com>
2023-05-22 12:43:21 +0800

    fix: package name: dnsmasq-full -> dnsmasq

--- sdk.sh
+++ sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make defconfig
 # IGNORE_ERRORS=1
-make -j$(nproc) package/luci-app-passwall/compile package/dnsmasq-full/compile || make -j1 package/luci-app-passwall/compile package/dnsmasq-full/compile V=s
+make -j$(nproc) package/luci-app-passwall/compile package/dnsmasq/compile || make -j1 package/luci-app-passwall/compile package/dnsmasq/compile V=s
