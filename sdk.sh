4ba5e80f86d29dce0fd1d058397a9368013acace
Yiffyi Jia <daniel_sy@live.com>
2023-05-22 02:25:04 +0000

    fix: .config overwritten by feeds update

--- sdk.sh
+++ sdk.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
-echo Before defconfig
-cat .config
 
+mv diffconfig .config
 make defconfig
-
-echo After defconfig
-cat .config
 
 # IGNORE_ERRORS=1
 make -j$(nproc) package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
