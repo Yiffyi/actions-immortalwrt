2f79c7436dccaaf0138a96f1b8a6f558bf30e5f1
Yiffyi Jia <daniel_sy@live.com>
2023-05-22 02:13:39 +0000

    what happend?

--- sdk.sh
+++ sdk.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 ./scripts/feeds update -a && ./scripts/feeds install luci-app-passwall
-make defconfig
+echo Before defconfig
 cat .config
+
+make defconfig
+
+echo After defconfig
+cat .config
+
 # IGNORE_ERRORS=1
 make -j$(nproc) package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
