d08496ea5536beb56ad947197c4875ce56f6fb1b
Yiffyi Jia <daniel_sy@live.com>
2023-05-23 01:07:26 +0800

    add: nft kmods

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq dnsmasq-full luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq dnsmasq-full kmod-nft-socket kmod-nft-tproxy kmod-nft-nat luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
