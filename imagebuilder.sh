12c7b3e3f198e265f02b26fb568172e6995692ae
Yiffyi Jia <daniel_sy@live.com>
2023-05-22 04:17:27 +0000

    try: openwrt-22.03.5

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq dnsmasq-full ipt2socks iptables-mod-iprange iptables-mod-tproxy luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua -dnsmasq dnsmasq-full luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
--- sdk.sh
+++ sdk.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./scripts/feeds install luci-app-passwall
 make defconfig
 # IGNORE_ERRORS=1
-make -j$(nproc) package/luci-app-passwall/compile || make -j1 package/luci-app-passwall/compile V=s
+make -j$(nproc) package/luci-app-passwall/compile package/dnsmasq-full/compile || make -j1 package/luci-app-passwall/compile package/dnsmasq-full/compile V=s
