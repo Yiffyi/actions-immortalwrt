b29a89fb3ab99a4027aa205367ddfaaf75e2b406
Yiffyi Jia <daniel_sy@live.com>
2023-05-24 17:09:46 +0800

    use legacy iptables firewall

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua smartdns kmod-nft-socket kmod-nft-tproxy kmod-nft-nat luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn -dnsmasq dnsmasq-full -firewall4 firewall ipset ipt2socks iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
