1f657d45eb534c1eebd9e5eb48b903b92fbf059a
Yiffyi Jia <daniel_sy@live.com>
2023-05-25 11:30:47 +0000

    use: my backport

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="libubox-lua smartdns kmod-nft-socket kmod-nft-tproxy kmod-nft-nat luci luci-app-passwall luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn luci-i18n-passwall-zh-cn" FILES="files"
+pkgs = (
+    libubox-lua
+    smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn
+    luci-app-passwall luci-i18n-passwall-zh-cn kmod-nft-socket kmod-nft-tproxy kmod-nft-nat
+    luci luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn 
+)
+ 
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="${pkgs[@]}" FILES="files"
