1194fad038a7ff0058ad76e87dffa7e607d5a704
Yiffyi Jia <daniel_sy@live.com>
2023-05-25 11:05:54 +0000

    debug: print .config

--- sdk.sh
+++ sdk.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 make defconfig
+
+echo "Generated .config >>>"
+cat .config
+echo ".config end <<<"
+
 # IGNORE_ERRORS=1
 make -j$(nproc) package/luci-app-passwall/compile package/dnsmasq/compile || make -j1 package/luci-app-passwall/compile package/dnsmasq/compile V=s
