a22d15597f678f159f8f6002cac02f4bb75572e7
Yiffyi Jia <daniel_sy@live.com>
2023-05-25 11:33:57 +0000

    fix: script pkgs

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkgs = (
+pkgs=(
     libubox-lua
     smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn
     luci-app-passwall luci-i18n-passwall-zh-cn kmod-nft-socket kmod-nft-tproxy kmod-nft-nat
