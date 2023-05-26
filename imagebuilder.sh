66f34e268348b65521ea43e63fbc877b76752026
Yiffyi Jia <daniel_sy@live.com>
2023-05-26 12:14:23 +0800

    smartdns + dnsmasq-full
    dnsmasq-full required by passwall - nftables transparent proxy

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pkgs=(
     libubox-lua
+    -dnsmasq dnsmasq-full
     smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn
     luci-app-passwall luci-i18n-passwall-zh-cn kmod-nft-socket kmod-nft-tproxy kmod-nft-nat
     luci luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn 
