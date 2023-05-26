3e4f795a19e150702d75c2d242795688c5c647e7
Yiffyi Jia <daniel_sy@live.com>
2023-05-26 13:15:56 +0800

    revert: build all packages

--- sdk.sh
+++ sdk.sh
@@ -6,4 +6,4 @@ cat .config
 echo ".config end <<<"
 
 # IGNORE_ERRORS=1
-make -j$(nproc) || make -j1 V=s
+make -j$(nproc) package/{luci-app-passwall,dnsmasq,smartdns,luci-app-smartdns}/compile || make -j1 package/{luci-app-passwall,dnsmasq,smartdns,luci-app-smartdns}/compile V=s
