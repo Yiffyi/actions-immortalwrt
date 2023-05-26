e679b4364db019b467acf8bb91acf091fdb67d9d
Yiffyi Jia <daniel_sy@live.com>
2023-05-26 10:26:38 +0800

    fix: pkgs param

--- imagebuilder.sh
+++ imagebuilder.sh
@@ -5,5 +5,5 @@ pkgs=(
     luci-app-passwall luci-i18n-passwall-zh-cn kmod-nft-socket kmod-nft-tproxy kmod-nft-nat
     luci luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn 
 )
- 
-make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="${pkgs[@]}" FILES="files"
+
+make image PROFILE="xiaomi_redmi-router-ax6s" PACKAGES="${pkgs[*]}" FILES="files"
