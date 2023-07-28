817ab6badb73a16ce40267813a8ac12e31436514
Yiffyi Jia <daniel_sy@live.com>
2023-07-28 20:20:27 +0800

    Update imagebuilder-nx30pro.sh

--- imagebuilder-nx30pro.sh
+++ imagebuilder-nx30pro.sh
@@ -2,6 +2,7 @@
 pkgs=(
     libubox-lua
     -dnsmasq dnsmasq-full
+    tailscale
     smartdns luci-app-smartdns luci-i18n-smartdns-zh-cn
     luci-app-passwall luci-i18n-passwall-zh-cn kmod-nft-socket kmod-nft-tproxy kmod-nft-nat
     luci luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn 
