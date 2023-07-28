4e8c0a4967953aacc49237bf9eba8b65b783ef8c
Yiffyi Jia <daniel_sy@live.com>
2023-07-28 22:12:55 +0800

    use: bigger ubi partition for nx30pro

--- imagebuilder-nx30pro.sh
+++ imagebuilder-nx30pro.sh
@@ -8,4 +8,6 @@ pkgs=(
     luci luci-i18n-base-zh-cn luci-i18n-opkg-zh-cn luci-i18n-firewall-zh-cn 
 )
 
+cp $GITHUB_WORKSPACE/mt7981b-h3c-magic-nx30-pro-112M.dts target/linux/mediatek/dts/mt7981b-h3c-magic-nx30-pro.dts
+
 make image PROFILE="h3c_magic-nx30-pro" PACKAGES="${pkgs[*]}" FILES="files"
